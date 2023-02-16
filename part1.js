d673bce31415e656b586ef5ca5bdd5853229181a
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-02-16 12:32:55 +0100

    commit 2

--- part1.js
+++ part1.js
@@ -84,3 +84,12 @@ function merge(leftArr, rightArr) {
 }
 
 console.log(mergeSort(arr));
+
+function fib(n){
+    if (n >= 0 && n <=1){
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
+console.log(fib(6));
